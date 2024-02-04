ac7fd77ea4752d737ca46d1e755eef4a74ae6bc7
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-02-04 22:37:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Binance API credentials
+api_key = 'your_api_key'
+api_secret = 'your_api_secret'
+
+# Trading pairs and fixed quantity in USDT
+symbols = ['BTC/USDT', 'ETH/USDT', 'BNB/USDT']
+fixed_quantity_usdt = 100
